5e8822270778955d6ff0ea76520473e46e99e921
Sariel RAQUIN <sarielr@hotmail.fr>
2023-02-03 16:47:55 +0100

    add some insertions into tables

--- file.sql
+++ file.sql
@@ -1,6 +1,10 @@
+-- Creation de la base de données --
+
 CREATE DATABASE Theater;
 
 USE Theater;
+
+-- Creation des tables -- 
 
 CREATE TABLE users (
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -16,46 +20,150 @@ CREATE TABLE users (
 
 
 CREATE TABLE customers (
-    users_id INT(11) NOT NULL PRIMARY KEY,
-    FOREIGN KEY (users_id) REFERENCES users(id)
+    status VARCHAR(10) NOT NULL,
+    customers_id INT(11) NOT NULL PRIMARY KEY,
+    FOREIGN KEY (customers_id) REFERENCES users(id)
+    
 
 )    ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE admins (
-    users_id INT(11) NOT NULL PRIMARY KEY,
-    FOREIGN KEY (users_id) REFERENCES users(id)
+    admins_id INT(11) NOT NULL PRIMARY KEY,
+    FOREIGN KEY (admins_id) REFERENCES users(id)
     
 )   ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 
 
 CREATE TABLE cinemas (
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    cinemas_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
     adress VARCHAR(250) NOT NULL,
     zipcode VARCHAR(5) NOT NULL,
     city VARCHAR(60) NOT NULL,
     phone int(10) NOT NULL,
-    admins_id INT(11) NOT NULL,
-    FOREIGN KEY (admins_id) REFERENCES admins(users_id)
+    admins_id INT(11),
+    FOREIGN KEY (admins_id) REFERENCES admins(admins_id)
 
 )   ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE moviesroom (
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    movies_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     number_rooms INT(11),
     place_capacity INT(11),
     cinemas_id INT(11) NOT NULL,
-    FOREIGN KEY (cinemas_id) REFERENCES cinemas(id)
+    FOREIGN KEY (cinemas_id) REFERENCES cinemas(cinemas_id)
     
 
 )   ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE projections (
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    projections_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     projection_time DATETIME,
     moviesroom_id INT(11) NOT NULL,
-    FOREIGN KEY (moviesroom_id) REFERENCES moviesroom(id)
+    FOREIGN KEY (moviesroom_id) REFERENCES moviesroom(movies_id)
 )   ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE genre (
+    genre_id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    libelle VARCHAR(60) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE movies (
+    movies_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(60) NOT NULL,
+    duration TIME NOT NULL,
+    release_date DATETIME NOT NULL,
+    synopsis TEXT NOT NULL,
+    directed_by VARCHAR(20),
+    projections_id INT(11) NOT NULL,
+    genre_id INT(11) NOT NULL,
+    FOREIGN KEY (projections_id) REFERENCES projections(projections_id),
+    FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
+)   ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE booking (
+    booking_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    movie VARCHAR(60) NOT NULL,
+    date_reservation DATETIME NOT NULL,
+    type_payment VARCHAR(20),
+    total_price INT(11) NOT NULL,
+    date_payment DATETIME NOT NULL,
+    place_quantity INT(11) NOT NULL,
+    projections_id INT(11) NOT NULL,
+    customers_id INT(11) NOT NULL,
+    FOREIGN KEY (projections_id) REFERENCES projections(projections_id),
+    FOREIGN KEY (customers_id) REFERENCES customers(customers_id)
+)   ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- insertion des données dans le base de données --
+
+insert into users (first_name, last_name, date_of_birth, email, pseudo, password) values ('Sondra', 'Neeves', '1980/12/01', 'sneeves0@storify.fr', 'sneeves0', 'sH80nYGrRrJ');
+insert into users (first_name, last_name, date_of_birth, email, pseudo, password) values ('Brittne', 'Bourdel', '1982/11/02', 'bbourdel1@about.fr', 'bbourdel1', 'BQnJbikTgAE');
+insert into users (first_name, last_name, date_of_birth, email, pseudo, password) values ('Catina', 'Farrears', '1970/09/05', 'cfarrears2@ucla.fr', 'cfarrears2', 'plBHmQz');
+insert into users (first_name, last_name, date_of_birth, email, pseudo, password) values ('Duane', 'Baldazzi', '1985/08/01', 'dbaldazzi3@unesco.fr', 'dbaldazzi3', 'hw74L9Dn2GP');
+insert into users (first_name, last_name, date_of_birth, email, pseudo, password) values ('Lissy', 'Cuerda', '1985/07/01', 'lcuerda4@arizona.fr', 'lcuerda4', '7R861WPu');
+insert into users (first_name, last_name, date_of_birth, email, pseudo, password) values ('Uri', 'Brigstock', '1985/12/01', 'ubrigstock5@ning.fr', 'ubrigstock5', 'ueXIeslrrIFd');
+
+
+insert into customers (customers_id,status) values (2 , 'Etudiant');
+insert into customers (customers_id,status) values (4 , 'Normal');
+insert into customers (customers_id,status) values (6 , 'Enfant');
+
+insert into admins (admins_id) values (3) , (5);
+
+
+insert into cinemas (name, adress, zipcode, city, phone, admins_id) values ('Feest and Sons', '3rd Floor', '98231', 'Zduńska Wola', '4117381399',3);
+insert into cinemas (name, adress, zipcode, city, phone, admins_id) values ('Corwin, Maggio and Blanda', 'Suite 62', '22555', 'Vitomarci', '2654800831',5);
+insert into cinemas (name, adress, zipcode, city, phone) values ('Reynolds and Sons', 'Room 1994', "69800", 'Condado', '8189994525');
+insert into cinemas (name, adress, zipcode, city, phone) values ('Kreiger-Howe', 'Apt 663', '87457', 'Bhātpāra Abhaynagar', '3716830535');
+insert into cinemas (name, adress, zipcode, city, phone) values ('Kessler-Brakus', 'PO Box 88553', '29002', 'Maricá', '8814487488');
+insert into cinemas (name, adress, zipcode, city, phone) values ('Nolan and Sons', '12th Floor', '69740', 'Kanália', '4697144079');
+
+
+
+
+insert into moviesroom (number_rooms, place_capacity, cinemas_id) values (1, 300,1);
+insert into moviesroom (number_rooms, place_capacity, cinemas_id) values (2, 320,3);
+insert into moviesroom (number_rooms, place_capacity, cinemas_id) values (3, 250,4);
+insert into moviesroom (number_rooms, place_capacity, cinemas_id) values (4, 400,6);
+insert into moviesroom (number_rooms, place_capacity, cinemas_id) values (5, 150,2);
+insert into moviesroom (number_rooms, place_capacity, cinemas_id) values (6, 200,5);
+
+
+insert into projections (projection_time,moviesroom_id) values ('2023/12/01 11:00:00',2);
+insert into projections (projection_time,moviesroom_id) values ('2023/12/02 12:00:00',1);
+insert into projections (projection_time,moviesroom_id) values ('2023/12/03 13:00:00',4);
+insert into projections (projection_time,moviesroom_id) values ('2023/12/04 14:00:00',3);
+insert into projections (projection_time,moviesroom_id) values ('2023/12/05 15:00:00',5);
+insert into projections (projection_time,moviesroom_id) values ('2023/12/06 16:00:00',6);
+
+
+insert into genre (libelle) values ('Drama|War');
+insert into genre (libelle) values ('Action|Adventure|Thriller');
+insert into genre (libelle) values ('Adventure|Drama');
+insert into genre (libelle) values ('Crime|Drama|Mystery|Thriller');
+insert into genre (libelle) values ('Action|Animation|Children|Comedy|Fantasy');
+insert into genre (libelle) values ('Action|Drama|Mystery');
+
+
+
+insert into movies (title, duration, release_date, synopsis, directed_by, projections_id, genre_id) values ('Carla''s Song', '3:17', '1980/03/06', 'quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae duis faucibus accumsan odio curabitur convallis duis consequat dui nec nisi volutpat eleifend donec ut dolor morbi vel lectus in quam fringilla rhoncus mauris enim leo rhoncus sed', 'Danice Hast',1,2);
+insert into movies (title, duration, release_date, synopsis, directed_by, projections_id, genre_id) values ('Prince Valiant', '2:09', '1970/04/05', 'aliquam augue quam sollicitudin vitae consectetuer eget rutrum at lorem integer tincidunt ante vel ipsum praesent blandit lacinia erat vestibulum sed magna at nunc commodo placerat praesent blandit nam nulla integer pede justo lacinia eget tincidunt eget tempus vel pede morbi', 'Averill Jurries',2,3);
+insert into movies (title, duration, release_date, synopsis, directed_by, projections_id, genre_id) values ('Fullmetal Alchemist', '1:55', '1950/12/12', 'primis in faucibus orci luctus et ultrices posuere cubilia curae duis faucibus accumsan odio curabitur convallis duis consequat dui nec nisi volutpat eleifend donec ut dolor morbi vel lectus in quam fringilla rhoncus mauris enim leo rhoncus sed vestibulum sit amet cursus id turpis integer aliquet', 'Trever Goulbourne',5,6);
+insert into movies (title, duration, release_date, synopsis, directed_by, projections_id, genre_id) values ('Boiler Room', '0:46', '1940/02/08', 'ac diam cras pellentesque volutpat dui maecenas tristique est et tempus semper est quam pharetra magna ac consequat metus sapien ut nunc vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae mauris', 'Cacilia Cornthwaite',3,4);
+insert into movies (title, duration, release_date, synopsis, directed_by, projections_id, genre_id) values ('Life as a House', '3:43', '1965/04/03', 'nec condimentum neque sapien placerat ante nulla justo aliquam quis turpis eget elit sodales scelerisque mauris sit amet eros suspendisse accumsan tortor quis turpis sed ante vivamus tortor duis mattis egestas metus aenean fermentum donec ut mauris eget massa tempor convallis nulla neque libero', 'Erie Cantillion',4,1);
+insert into movies (title, duration, release_date, synopsis, directed_by, projections_id, genre_id) values ('Hot Spot, The', '6:43', '1975/12/01', 'amet consectetuer adipiscing elit proin risus praesent lectus vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia', 'Lucien Shinfield',6,5);
+
+
+
+
+insert into booking (movie, date_reservation, type_payment, total_price, date_payment, place_quantity) values ('Too Late the Hero', '2023/05/11', 'Nelco Laboratories, Inc.', 'PF', '2023/02/03', 1);
+insert into booking (movie, date_reservation, type_payment, total_price, date_payment, place_quantity) values ('Breath, The (Nefes: Vatan sagolsun)', '2023/06/11', 'Banner Pharmacaps Inc', 'US', '2023/02/03', 2);
+insert into booking (movie, date_reservation, type_payment, total_price, date_payment, place_quantity) values ('300', '2023/07/11', 'Mycone Dental Supply Co., Inc DBA Keystone Industries and Deepak Products Inc.', 'TW', '2023/02/03', 3);
+insert into booking (movie, date_reservation, type_payment, total_price, date_payment, place_quantity) values ('Pope''s Toilet', '2023/08/11', 'Cardinal Health Inc.', 'CA', '2023/02/03', 4);
+insert into booking (movie, date_reservation, type_payment, total_price, date_payment, place_quantity) values ('Sleeping Car Murder', '2023/04/11', 'Professional Disposables International, Inc.', 'CN', '2023/02/03', 5);
+insert into booking (movie, date_reservation, type_payment, total_price, date_payment, place_quantity) values ('Charlie Chan in Dangerous Money', '2023/03/11', 'Amerisource Bergen', 'DE', '4/17/2022', 6);
